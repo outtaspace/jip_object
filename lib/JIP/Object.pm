28293ad4642279b45b9a888d9d4f4f24e3fcd87d
outtaspace <flyweight@yandex.ru>
2015-07-14 11:15:20 +0300

    Added POD

--- lib/JIP/Object.pm
+++ lib/JIP/Object.pm
@@ -177,3 +177,121 @@ JIP::ClassField::cleanup_namespace(qw(has croak blessed));
 
 1;
 
+__END__
+
+=head1 NAME
+
+JIP::Object - A simple object system.
+
+=head1 VERSION
+
+Version 0.01
+
+=head1 SYNOPSIS
+
+    use Test::More;
+    use JIP::Object;
+
+    my $obj = JIP::Object->new;
+    ok $obj, 'got object';
+
+    # Public access to the "foo"
+    $obj->attr('foo', (get => '+', set => '+'));
+    is($obj->set_foo(42)->foo, 42);
+
+    # Private access to the "bar"
+    $obj->attr('bar', (get => '-', set => '-'));
+    is($obj->_set_bar(42)->_bar, 42);
+
+    # Create user-defined names for getters/setters
+    $obj->attr('wtf' => (get => 'wtf_getter', set => 'wtf_setter'));
+    is($obj->wtf_setter(42)->wtf_getter, 42);
+
+    # Pass an optional first argument of setter to set
+    # a default value, it should be a constant or callback.
+    $obj->attr('baz' => (get => '+', set => '+', default => 42));
+    is($self->set_baz->baz, 42);
+
+    $obj->attr('qux' => (get => '+', set => '+', default => sub {
+        my $self = shift;
+        return $self->baz;
+    }));
+    is($self->set_qux->qux, 42);
+
+    # Define a new method
+    $obj->method('say_foo', sub {
+        my $self = shift;
+        print $self->foo, "\n";
+    });
+
+    done_testing();
+
+=head1 METHODS
+
+=head2 new
+
+    my $obj = JIP::Object->new;
+
+=head2 attr
+
+    $obj = $obj->attr('x', get => 'x', set => 'set_x');
+
+=head2 method
+
+    $obj = $obj->method('get_x', sub {
+        my $self = shift;
+        return $self->x;
+    });
+
+=head1 SEE ALSO
+
+Mock::Quick::Object.
+
+=head1 AUTHOR
+
+Vladimir Zhavoronkov, C<< <flyweight at yandex.ru> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Vladimir Zhavoronkov.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+=cut
+
+
